27666e4e5d6cb90af38bc76b0ed42f3a4e8cf4f4
Takagi Kensuke <janus.wel.3@gmail.com>
2019-09-17 02:42:21 +0900

    :wrench::arrow_up: update outdated tools

--- src/util/deep-copy.ts
+++ src/util/deep-copy.ts
@@ -1,2 +1,2 @@
-const deepCopy = <T extends Array<any> | {}>(target: T): T => JSON.parse(JSON.stringify(target))
+const deepCopy = <T extends any[] | {}>(target: T): T => JSON.parse(JSON.stringify(target))
 export default deepCopy
