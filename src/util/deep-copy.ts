2a9f5026279e92066796cb2968caaea666f204d1
Takagi Kensuke <janus.wel.3@gmail.com>
2020-05-17 16:39:53 +0900

    :wrench::art::arrow_up: configure ESLint

--- src/util/deep-copy.ts
+++ src/util/deep-copy.ts
@@ -1,2 +1,9 @@
-const deepCopy = <T extends any[] | {}>(target: T): T => JSON.parse(JSON.stringify(target))
-export default deepCopy
+type JSONSerializable = number | string | boolean | null | JSONSerializableObject | JSONSerializableArray
+type JSONSerializableObject = JSONSerializableObjectStringKey | JSONSerializableObjectNumberKey
+type JSONSerializableObjectStringKey = { [key: string]: JSONSerializable }
+type JSONSerializableObjectNumberKey = { [key: number]: JSONSerializable }
+type JSONSerializableArray = Array<JSONSerializable>
+
+export default function deepCopy(target: JSONSerializable): JSONSerializable {
+  return JSON.parse(JSON.stringify(target))
+}
